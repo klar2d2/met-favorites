e94ec48da9cb77bd00d059ec9aba74743bbc17d0
klar2d2 <koberyan@comcast.net>
2019-08-28 13:10:53 -0700

    working API call and res.render

--- controllers/search.js
+++ controllers/search.js
@@ -4,6 +4,7 @@ const router = express.Router();
 process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser;
 // const fetch = require('node-fetch')
 const axios = require('axios')
+const async = require('async')
 
 
 
@@ -11,22 +12,43 @@ router.get('/', (req, res) => {
   console.log('here')
   res.render('search/index')
 })
-
+// To get the object ID's passed to the artwork API for images etc.
 router.get('/results', (req, res) => {
-  axios.get('https://collectionapi.metmuseum.org/public/collection/v1/search')
+  //get query details from the form
+  axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=${req.query.highlight}&isOnView=${req.query.view}&q=${req.query.keyname}&perPage=20`)
   .then(response => {
-      let data = response.data
-      console.log(data)
+      let result = {}
+      // Get objectID's from call one
+      async.forEach(response.data.objectIDs, function(work, cb) { //work is an ID
+        //Taking the ID's and indiviually going to get information for the second API call in Parallel
+        axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${work}`)
+        .then(response2 => {
+          //storing the info from the ID key (work) response so we can have it later.
+          result[work] = response2.data
+          cb()
+        })
+        .catch((err) => {
+          console.log("error in async foreach call", err)
+          cb()
+        })
+      }, () => { //argument 3 for the async foreach
+        console.log('All done', result)
+        res.render('search/results', {result})
+      }); // END OF FOREACH
+
     }).catch((err) => {
       console.log("err", err)
+      res.render('404')
     })
+
 })
 
 
 
 
 router.get('/results/:id', (req, res) => {
-  res.send('This is a stub for when the User wants specific information on a result')
+  console.log(req.params.id)
+  res.send('stub')
 })
 
 router.post('/router', (req, res) => {
